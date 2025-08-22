23b45cc7326b6a2dd2936b4d5dfc9ba156789f0f
Fresik1 <freskotwich@gmail.com>
2025-08-23 00:32:19 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+API_TOKEN = '8263662464:AAH96vPWKs3Nokal8IehFK30dm0mZs_1RtQ'
+
+admin = [1708345481]
+start_money = 10000
+
+bot_name = 'FB'
+chat = 't.me/FarmingBotFB'
+channel = 't.me/FBInvestorChannel'
+admin_username = '@FreskoWIN'
+bot_username = 'FB'
+
+chat_log = 0
+cleaning = 60
+
+custom_modules = True
